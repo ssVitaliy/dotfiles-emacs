ff40b2894dc3043680fbc719d55e671bc6c8d462
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-07-19 14:00:46 +0300

    Pretty initial config
    ; Define my minor mode:
    define-minor-mode vskeys-mode
    
    ; Used funcs:
    vs-new-line-from-anyware
    vs-select-current-line-and-forward-line
    xah-extend-selection
    xah-search-current-word
    
    ; Used packages:
    vertico
    marginalia
    orderless
    consult
    company
    org

---
+++ init.el
@@ -0,0 +1,313 @@
+;;; Used packages:
+;; vertico
+;; marginalia
+;; orderless
+;; consult
+;; company
+;; org
+
+
+(setq default-directory "~/")
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(setq delete-by-moving-to-trash t)
+(setq use-short-answers t)
+(setq set-mark-command-repeat-pop t)
+(setq bookmark-save-flag 1)
+(setq auto-save-default nil)
+;; Do not create backup files
+(setq make-backup-files nil)
+
+;; Theme
+;(load-theme 'modus-operandi)            ; Light theme
+(load-theme 'modus-vivendi)             ; Dark theme
+
+(defun vs-new-line-from-anyware ()
+  "Jump to newline from anywhare in line"
+  (interactive)
+  (move-end-of-line 1)
+  (newline))
+
+(defun vs-select-current-line-and-forward-line (arg)
+  "Select the current line and move the cursor by ARG lines IF
+no region is selected.
+
+If a region is already selected when calling this command, only move
+the cursor by ARG lines."
+  (interactive "p")
+  (when (not (use-region-p))
+    (forward-line 0)
+    (set-mark-command nil))
+  (forward-line arg))
+
+(defun xah-extend-selection ()
+  "Select the current word, bracket/quote expression, or expand selection.
+Subsequent calls expands the selection.
+
+when there is no selection,
+• If cursor is on any type of bracket (including parenthesis, quotation mark), select whole bracketed thing including bracket
+• else, select current word.
+
+when there is a selection, the selection extension behavior is still experimental. But when cursor is on a any type of bracket (parenthesis, quote), it extends selection to outer bracket.
+
+URL `http://xahlee.info/emacs/emacs/emacs_extend_selection.html'
+Version: 2020-02-04 2023-08-24 2023-11-14"
+  (interactive)
+
+  (cond
+   ((region-active-p)
+    (let ((xp1 (region-beginning)) (xp2 (region-end)))
+      (goto-char xp1)
+      (cond
+       ((looking-at "\\s(")
+        (if (eq (nth 0 (syntax-ppss)) 0)
+            (progn
+              ;; (message "debug: left bracket, depth 0.")
+              (end-of-line) ; select current line
+              (push-mark (line-beginning-position) t t))
+          (progn
+            ;; (message "debug: left bracket, depth not 0")
+            (up-list -1 t t)
+            (mark-sexp))))
+       ((eq xp1 (line-beginning-position))
+        (progn
+          (goto-char xp1)
+          (let ((xfirstLineEndPos (line-end-position)))
+            (cond
+             ((eq xp2 xfirstLineEndPos)
+              (progn
+                ;; (message "debug: exactly 1 line. extend to next whole line." )
+                (forward-line 1)
+                (end-of-line)))
+             ((< xp2 xfirstLineEndPos)
+              (progn
+                ;; (message "debug: less than 1 line. complete the line." )
+                (end-of-line)))
+             ((> xp2 xfirstLineEndPos)
+              (progn
+                ;; (message "debug: beginning of line, but end is greater than 1st end of line" )
+                (goto-char xp2)
+                (if (eq (point) (line-end-position))
+                    (progn
+                      ;; (message "debug: exactly multiple lines" )
+                      (forward-line 1)
+                      (end-of-line))
+                  (progn
+                    ;; (message "debug: multiple lines but end is not eol. make it so" )
+                    (goto-char xp2)
+                    (end-of-line)))))
+             (t (error "%s: logic error 42946" real-this-command))))))
+       ((and (> (point) (line-beginning-position)) (<= (point) (line-end-position)))
+        (progn
+          ;; (message "debug: less than 1 line" )
+          (end-of-line) ; select current line
+          (push-mark (line-beginning-position) t t)))
+       (t
+        ;; (message "debug: last resort" )
+        nil))))
+
+   ((looking-at "\\s(")
+    ;; (message "debug: left bracket")
+    (mark-sexp))
+
+   ((looking-at "\\s)")
+    ;; (message "debug: right bracket")
+    (backward-up-list) (mark-sexp))
+
+   ((looking-at "\\s\"")
+    ;; (message "debug: string quote")
+    (mark-sexp))
+
+   ((looking-at "[ \t\n]")
+    ;; (message "debug: is white space")
+    (skip-chars-backward " \t\n")
+    (push-mark)
+    (skip-chars-forward " \t\n")
+    (setq mark-active t))
+
+   ((looking-at "[-_a-zA-Z0-9]")
+    ;; (message "debug: left is word or symbol")
+    (skip-chars-backward "-_a-zA-Z0-9")
+    (push-mark)
+    (skip-chars-forward "-_a-zA-Z0-9")
+    (setq mark-active t))
+
+   ((and (looking-at "[:blank:]")
+         (prog2 (backward-char) (looking-at "[:blank:]") (forward-char)))
+    ;; (message "debug: left and right both space" )
+    (skip-chars-backward "[:blank:]") (push-mark (point) t t)
+    (skip-chars-forward "[:blank:]"))
+
+   ((and (looking-at "\n")
+         (eq (char-before) 10))
+    ;; (message "debug: left and right both newline")
+    (skip-chars-forward "\n")
+    (push-mark (point)  t t)
+    (re-search-forward "\n[ \t]*\n"))
+
+   (t
+    ;; (message "debug: just mark sexp" )
+    (mark-sexp)
+    (exchange-point-and-mark))))
+
+(defun xah-search-current-word ()
+  "Call `isearch' on current word or selection.
+“word” here is A to Z, a to z, and hyphen [-] and lowline [_], independent of syntax table.
+
+URL `http://xahlee.info/emacs/emacs/modernization_isearch.html'
+Created: 2010-05-29
+Version: 2025-02-05"
+  (interactive)
+  (let (xbeg xend)
+    (if (region-active-p)
+        (setq xbeg (region-beginning) xend (region-end))
+      (save-excursion
+        (skip-chars-backward "-_A-Za-z0-9")
+        (setq xbeg (point))
+        (right-char)
+        (skip-chars-forward "-_A-Za-z0-9")
+        (setq xend (point))))
+    (when (< xbeg (point)) (goto-char xbeg))
+    (isearch-mode t)
+    (isearch-yank-string (buffer-substring-no-properties xbeg xend))))
+
+
+;; Keybindings
+(define-minor-mode vskeys-mode
+  "Minor mode with my keys."
+  :global t
+  :lighter " vskeys"
+  :keymap (let ((vskeys-mode-map (make-sparse-keymap)))
+  (keymap-set vskeys-mode-map "S-<return>" #'vs-new-line-from-anyware)
+  (keymap-set vskeys-mode-map "M-l" #'vs-select-current-line-and-forward-line)
+  (keymap-set vskeys-mode-map "M-8" #'xah-extend-selection)
+  (keymap-set vskeys-mode-map "M-s M-s" #'xah-search-current-word)
+
+  ;;; switch window, another window, next-window
+  (keymap-set vskeys-mode-map "M-2" #'other-window)
+  ;;; split window vertically
+  (keymap-set vskeys-mode-map "M-3" #'split-window-right)
+  ;;; delete window
+  (keymap-set vskeys-mode-map "M-0 M-0" #'delete-window)
+  ;;; delete other windows
+  (keymap-set vskeys-mode-map "M-0 M--" #'delete-other-windows)
+
+  (keymap-set vskeys-mode-map "C-v" #'yank)
+  (keymap-set vskeys-mode-map "C-z" #'undo)
+  (keymap-set vskeys-mode-map "C-S-z" #'undo-redo)
+  (keymap-set vskeys-mode-map "C-." #'kmacro-end-and-call-macro)
+  (keymap-set vskeys-mode-map "C-c i" #'point-to-register)
+  (keymap-set vskeys-mode-map "C-c j" #'pop-global-mark)
+  (keymap-set vskeys-mode-map "C-x C-k" 'kill-current-buffer)
+  ;(keymap-set vskeys-mode-map "M-n" 'forward-paragraph)
+  ;(keymap-set vskeys-mode-map "M-p" 'backward-paragraph)
+
+    vskeys-mode-map))
+(vskeys-mode t)
+
+
+;; grep config
+(setq grep-command "grep  -nH --null -i ")
+
+;; ISearch config
+(setq isearch-lazy-count t)
+(setq lazy-count-prefix-format "(%s/%s) ")
+(setq lazy-count-suffix-format nil)
+(setq search-whitespace-regexp ".*?")
+
+;; Dired
+(file-name-shadow-mode 1)
+(add-hook 'dired-mode-hook 'dired-omit-mode)
+;;; Automatically hide the detailed listing when visiting a Dired
+;;; buffer.  This can always be toggled on/off by calling the
+;;; `dired-hide-details-mode' interactively with M-x or its keybindings
+;;; (the left parenthesis by default).
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+(setq dired-kill-when-opening-new-dired-buffer t)
+
+;;; When there are two Dired buffers side-by-side make Emacs
+;;; automatically suggest the other one as the target of copy or rename
+;;; operations.  Remember that you can always use M-p and M-n in the
+;;; minibuffer to cycle through the history, regardless of what this
+;;; does.  (The "dwim" stands for "Do What I Mean".)
+(setq dired-dwim-target t)
+
+(setq package-archives
+	'(("gnu" . "https://elpa.gnu.org/packages/")
+	  ("melpa" . "https://melpa.org/packages/")
+	  ("elpy" . "http://jorgenschaefer.github.io/packages/")
+	  ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+(use-package vertico
+  :config
+  (setq vertico-cycle t)
+  (setq vertico-resize nil)
+  (vertico-mode 1))
+
+;; The built-in `savehist-mode' saves minibuffer histories.  Vertico
+;; can then use that information to put recently selected options at
+;; the top.
+(savehist-mode 1)
+
+;; tidy directory, clear old path
+(add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
+
+(use-package marginalia
+  :config
+  (marginalia-mode 1))
+
+(use-package orderless
+  :config
+  (setq completion-styles '(orderless basic)))
+
+(use-package consult
+  :config
+  (setq register-preview-delay 0.8
+	register-preview-function #'consult-register-format)
+
+  :bind (;; A recursive grep
+         ("M-s M-g" . consult-grep)
+         ;; Search for files names recursively
+         ("M-s M-f" . consult-find)
+         ;; Search through the outline (headings) of the file
+         ("M-s M-o" . consult-outline)
+         ;; Search the current buffer
+         ("M-s M-l" . consult-line)
+         ;; Switch to another buffer, or bookmarked file, or recently
+         ;; opened file.
+         ("M-s M-b" . consult-buffer)))
+
+(use-package company
+  :init
+  (global-company-mode)
+  :config
+  (company-mode)
+  (setq company-idle-delay 0)
+  (setq company-global-modes '(not processing-mode text-mode)) ;; Not use company on those modes
+  (add-to-list 'company-backends 'company-dabbrev) ;; Backend for header files
+  :bind (:map company-search-map  
+              ("C-t" . company-search-toggle-filtering)
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)
+              :map company-active-map
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)))
+
+; Org mode
+(use-package org
+  :config 
+  (setq org-log-done 'time)
+	   
+  :bind (
+	 :map org-mode-map
+	      ("C-c C-'" . org-mark-ring-goto)
+	      ("C-c C-;" . org-mark-ring-push)))
+
+
+;; ;; denote package
+;; (setq denote-directory (expand-file-name "~/odis-api/"))
+;; (setq denote-known-keywords '("odisapi"))
+;; (add-hook 'dired-mode-hook #'denote-dired-mode)
+
