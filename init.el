0a9b46ac6b83191d44d5fc06590461f6e7246066
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-07-24 14:34:28 +0300

    feat: Toggle window split from horizontal to vertical.

--- init.el
+++ init.el
@@ -423,3 +423,32 @@ Version: 2025-02-05"
 ;; (setq denote-known-keywords '("odisapi"))
 ;; (add-hook 'dired-mode-hook #'denote-dired-mode)
 
+;;; Toggle window split from horizontal to vertical.
+;; by JeffDwork https://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(keymap-set vskeys-mode-map "M-t" 'toggle-window-split)
+
