29c557b9105e9c5c511674ad1aa810e82af9de63
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-07-25 17:25:45 +0300

    feat: vdiff-magit with basic conf

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 ;; company-statistics
 ;; org
 ;; vdiff
+;; magit
+;; vdiff-magit
 
 
 ;;; Common setups
@@ -268,10 +270,36 @@ This includes major mode and other minor mode keybindings."
          ("M-2" . vdiff-switch-buffer)))
 
 
-;;; magit
+;;; magit + vdiff
 (use-package magit
   :config
   (keymap-unset magit-status-mode-map "M-2")
   (keymap-unset magit-status-mode-map "M-3")
   (keymap-unset magit-stash-mode-map "M-2")
   (keymap-unset magit-stash-mode-map "M-3"))
+
+
+(use-package vdiff-magit
+  :after magit
+  :config
+  (keymap-set magit-mode-map "e" 'vdiff-magit-dwim)
+  (keymap-set magit-mode-map "E" 'vdiff-magit)
+  (transient-suffix-put 'magit-dispatch "e" :description "vdiff (dwim)")
+  (transient-suffix-put 'magit-dispatch "e" :command 'vdiff-magit-dwim)
+  (transient-suffix-put 'magit-dispatch "E" :description "vdiff")
+  (transient-suffix-put 'magit-dispatch "E" :command 'vdiff-magit)
+  ;; This flag will default to using ediff for merges.
+  ;; (setq vdiff-magit-use-ediff-for-merges nil)
+
+  ;; Whether vdiff-magit-dwim runs show variants on hunks.  If non-nil,
+  ;; vdiff-magit-show-staged or vdiff-magit-show-unstaged are called based on what
+  ;; section the hunk is in.  Otherwise, vdiff-magit-dwim runs vdiff-magit-stage
+  ;; when point is on an uncommitted hunk.
+  ;; (setq vdiff-magit-dwim-show-on-hunks nil)
+
+  ;; Whether vdiff-magit-show-stash shows the state of the index.
+  ;; (setq vdiff-magit-show-stash-with-index t)
+
+  ;; Only use two buffers (working file and index) for vdiff-magit-stage
+  (setq vdiff-magit-stage-is-2way t)
+  )
